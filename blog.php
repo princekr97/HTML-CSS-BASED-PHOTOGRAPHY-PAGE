b51243280110007db7557546d1956b59c9a453c7
princekr97 <princekguptar97@gmail.com>
2019-10-16 14:58:31 +0530

    first commit

---
+++ blog.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="style.css">
+<style>
+	body  {
+    background-image: url("back1.jpg");
+    background-color: #cccccc;
+}
+.me {
+    border-radius: 100%;
+    width: 20em;  
+	height: 15em;
+    padding: 0em 2em;
+    float: left;
+}
+.intro {
+    line-height: 2em;
+}
+</style>
+</head>
+
+<body>
+
+<div class="topnav">
+  <a class="active" href="first.php">Home</a>
+  <a href="#">Blog</a>
+  <a href="third.html">Places</a>
+  <a href="fourth.html">Event</a>
+  <a href="fifth.html">Contact</a>
+</div>
+<br>
+
+<article class="intro">
+    <img src="sagar.jpg" class="me" />
+    <h2>Hi there.</h2>
+    <h3>Hi, I'm Sagar Gupta! If you are here, you are probably curious to know more about me. I am a boy who likes to travel. I liked to travel so much that I ended up quitting a well paying corporate job at the start of 2018 for it<h3> It may sound absurd, but through the wonders of the internet/ I have been able to create a sustainable job in travel for myself since I enjoy making travel related videos on YouTube</h3><h3> So no points for guessing that I have a YouTube channel which is all about travel. I am somebody who loves adventure, new cultures, different experiences and all good things that one can learn outside of the comfort of our homes I want to experience all that this beautiful earth has for us humans and document it along the way and share it with the YOU. 
+	<h3>If you want a detailed FAQ about me and my YouTube journey you can head to THIS page!
+ <a href="https://www.youtube.com/channel/UCSlTMfl-zn40AHLopjJ-CYg?view_as=subscriber">youtube</a>channel
+	and follow me on <a href="https://www.facebook.com/prince.krgupta.5">facebook</a>.<h3>
+</article>
+<article>
+</article>
+</body>
+</html>
